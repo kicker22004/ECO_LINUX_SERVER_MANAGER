05d0f92a41cbfb7f9cfc6210e12a62c996f9afec
kicker22004 <kicker22004@gmail.com>
2018-03-14 10:15:43 -0700

    Update update.sh

--- Files/update.sh
+++ Files/update.sh
@@ -8,43 +8,9 @@ CLEAR="tput sgr0"
 
 case "$1" in
    (*)
-    SELECTED_DIR=$2
+    SELECTED_DIR=$1
     ;;
 esac
-
-## This should stop the crash moving to ELSM 2.0
-if [ -z $SELECTED_DIR ]; then
-mkdir /opt/ELSM/ELSM2_Backup
-screen -X -S WATCH quit
-screen -X -S ECO quit
-sleep 5
-cp -a /opt/ELSM/Server/. /opt/ELSM/ELSM2_Backup/
-cp /opt/ELSM/Files/conf.cfg /opt/ELSM/ELSM2_Backup/
-cd /opt/ELSM/
-git clone https://github.com/kicker22004/ECO_LINUX_SERVER_MANAGER.git
-rm -rf /opt/ELSM/Files/*
-rm -rf /opt/ELSM/Server/*
-cd ECO_LINUX_SERVER_MANAGER
-sudo mv ELSM /usr/bin/ELSM
-cp Files/* /opt/ELSM/Files/
-cp -a /opt/ELSM/ELSM2_Backup/. /opt/ELSM/Server/ECO
-cp /opt/ELSM/Files/conf.cfg /opt/ELSM/Server/ECO/conf.cfg
-cat <<EOF >> changelog
-1. Your server was killed, I would NEVER do this if I could avoid it!.
-2. Multi-Server support! (make sure to select your ports correctly).
-3. I know this seems like a small changelog, But trust me it's a lot of time and work.
-EOF
-whiptail --fb --title "ELSM Changelog 0.1.5" --textbox changelog 20 60
-rm changelog
-source /opt/ELSM/ELSM2_Backup/conf.cfg
-sed -i "s/ECO_VERSION=.*/ECO_VERSION=$ECO_VERSION/g" /opt/ELSM/Server/ECO/conf.cfg
-sed -i "s/check=.*/check=$check/g" /opt/ELSM/Server/ECO/conf.cfg
-sed -i "s/startonboot=.*/startonboot=$startonboot/g" /opt/ELSM/Server/ECO/conf.cfg
-rm -rf /opt/ELSM/ECO_LINUX_SERVER_MANAGER
-rm -rf /opt/ELSM/ELSM2_Backup
-exit 1
-fi
-
 
 do_run() {
 DIR="/opt/ELSM/Server"
