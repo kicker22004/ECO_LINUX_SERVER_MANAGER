973b0fdca2fe385e0e5b748f78097be4b7ad79f3
kicker22004 <kicker22004@gmail.com>
2018-03-19 00:08:11 -0700

    Update update.sh

--- Files/update.sh
+++ Files/update.sh
@@ -15,17 +15,16 @@ esac
 do_run() {
 DIR="/opt/ELSM/Server"
 source $DIR/$SELECTED_DIR/conf.cfg
-echo $DIR/$SELECTED_DIR
+LOCK=$SELECTED_DIR
         cd $INSTALL_LOC
         wget -q https://raw.githubusercontent.com/kicker22004/ECO_Linux_Server_Manager/master/Files/conf.cfg -O conf.cfg
         source conf.cfg
-        echo $SELECTED_DIR
         find=$(grep "ELSM_VERSION" conf.cfg > tmp)
         find=$(cat tmp | cut -d "=" -f2)
         clear
         echo -e ${yellow}"Version found online: $find"
         unset ELSM_VERSION
-        source $DIR/$SELECTED_DIR/conf.cfg
+        source $DIR/$LOCK/conf.cfg
         echo "Currently Installed Version: $ELSM_VERSION"
 	echo -e ${green}"Created and Maintained by: Kicker22004"
         $CLEAR
@@ -48,13 +47,13 @@ else
         cd /opt/ELSM/Files/
         wget -q https://raw.githubusercontent.com/kicker22004/ECO_LINUX_SERVER_MANAGER/master/Files/upgrade -O upgrade
         chmod +x upgrade
-        /opt/ELSM/Files/upgrade "${SELECTED_DIR[@]}"
+        /opt/ELSM/Files/upgrade "${LOCK[@]}"
         echo -e ${green}"You are up to date."
         echo "Launching Program!"
         $CLEAR
         sleep 2
         exit 0
-        source $DIR/$SELETED_DIR/conf.cfg
+        source $DIR/$LOCK/conf.cfg
         exit 1
         /usr/bin/ELSM
 fi
