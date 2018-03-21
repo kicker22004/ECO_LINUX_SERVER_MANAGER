9981567fe9758b8e78bede4504c6f4b757bd2ecc
Valentin RIcard <redblueflame1@gmail.com>
2018-03-21 19:42:43 +0100

    New update for updater lol.

--- Files/update.sh
+++ Files/update.sh
@@ -11,51 +11,47 @@ case "$1" in
     SELECTED_DIR=$1
     ;;
 esac
-
-do_run() {
-DIR="/opt/ELSM/Server"
-source $DIR/$SELECTED_DIR/conf.cfg
-LOCK=$SELECTED_DIR
-    cd $INSTALL_LOC
-    wget -q https://raw.githubusercontent.com/$GIT_REPO_USER/ECO_LINUX_SERVER_MANAGER/$DEFAULT_BRANCH/Files/conf.cfg -O conf.cfg
-    source conf.cfg
-    find=$(grep "ELSM_VERSION" conf.cfg > tmp)
-    find=$(cat tmp | cut -d "=" -f2)
-    clear
-    echo -e ${yellow}"Version found online: $find"
-    unset ELSM_VERSION
-    source $DIR/$LOCK/conf.cfg
-    echo "Currently Installed Version: $ELSM_VERSION"
-    echo -e ${green}"Created and Maintained by: Kicker22004 and all the contributors <3"
-    $CLEAR
-if [ $find = $ELSM_VERSION ]; then
+do_run_app() {
     echo -e ${green}"You are up to date."
     echo "Launching Program!"
-    $CLEAR
     sleep 1
-    rm conf.*
-    rm tmp
     exit 0
     /usr/bin/ELSM
-else
-    rm conf.*
-    rm tmp
+}
+do_upgrade() {
     clear
     echo -e ${red}"An Updated Version was found, Grabbing files"
-    $CLEAR
     sleep 2
     cd /opt/ELSM/Files/
     wget -q https://raw.githubusercontent.com/$GIT_REPO_USER/ECO_LINUX_SERVER_MANAGER/$DEFAULT_BRANCH/Files/upgrade -O upgrade
     chmod +x upgrade
     /opt/ELSM/Files/upgrade "${LOCK[@]}"
-    echo -e ${green}"You are up to date."
-    echo "Launching Program!"
-    $CLEAR
-    sleep 2
-    exit 0
-    source $DIR/$LOCK/conf.cfg
-    exit 1
-    /usr/bin/ELSM
-fi
+    do_run_app
+}
+do_check_updates() {
+    SERVER_SHA=$(curl -s https://api.github.com/repos/${GITHUB_ORGANIZATION_NAME}/${REPO_NAME}/commits/master | jq '.sha' | sed 's/"//g');
+    LOCAL_SHA=$(<$DIR/Files/updater_data.cfg)
+    if [ $SERVER_SHA = $LOCAL_SHA ]; then
+        do_run_app
+    else
+        do_upgrade
+    fi
+}
+do_run() {
+#Dir variables, do not touch
+DIR="/opt/ELSM/Server"
+source $DIR/$SELECTED_DIR/conf.cfg
+LOCK=$SELECTED_DIR
+cd $INSTALL_LOC
+wget -q https://raw.githubusercontent.com/$GIT_REPO_USER/ECO_LINUX_SERVER_MANAGER/$DEFAULT_BRANCH/Files/conf.cfg -O conf.cfg
+source conf.cfg
+find=$(grep "ELSM_VERSION" conf.cfg > tmp)
+find=$(cat tmp | cut -d "=" -f2)
+clear
+echo -e ${yellow}"Version found online: $find"
+unset ELSM_VERSION
+source $DIR/$LOCK/conf.cfg
+echo "Currently Installed Version: $ELSM_VERSION"
+echo -e ${green}"Created and Maintained by: Kicker22004 and all the contributors <3"
 }
 do_run
